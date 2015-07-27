ceea2ccbe3b24db670c09821e858389163efbb62
Joe Lin <linjoeyy@gmail.com>
2015-07-27 00:00:30 -0400

    add term builder

---
+++ src/term.js
@@ -0,0 +1,59 @@
+/**
+ * Class Term
+ */
+var Term = (function() {
+  function _attachField (op, value, field) {
+    if (op !== undefined) {
+      this.queryText += op + '+';
+    }
+    this.queryText += value + '[' + field + ']+';
+  }
+
+  function _termConstructor(value, field) {
+    var _this = this;
+    _this.queryText = '';
+
+    if (field !== undefined && value !== undefined) {
+      _attachField.call(_this, undefined, value, field);
+    }
+
+    Object.defineProperty(_termConstructor.prototype, 'openParen', {
+      get: function() {
+        _this.queryText += '(';
+        return _this;
+      }
+    });
+
+    Object.defineProperty(_termConstructor.prototype, 'closeParen', {
+      get: function() {
+        _this.queryText = _this.queryText.substring(0, _this.queryText.length - 1);
+        _this.queryText += ')';
+        return _this;
+      }
+    });
+  };
+
+  _termConstructor.prototype.and = function(value, field) {
+    _attachField.call(this, 'AND', value, field);
+    return this;
+  };
+
+  _termConstructor.prototype.or = function(value, field) {
+    _attachField.call(this, 'OR', value, field);
+    return this;
+  };
+
+  _termConstructor.prototype.not = function(value, field) {
+    _attachField.call(this, 'not', value, field);
+    return this;
+  };
+  
+  _termConstructor.prototype.range = function(op, range, field) {
+    this.queryText += op + '+' + range[0] + '[' + field + ']:' + range[1] + '[' + field + ']+';
+    return this;
+  };
+
+  return _termConstructor;
+})();
+
+module.exports = Term;
